e27a4fa4e8b2bf2fcec15d4be30d3da54e750b53
Frederic Forjan <fforjan@free.fr>
2017-04-15 22:31:42 -0700

    better graph

--- src/dependenciesViewer/dependenciesDocument.ts
+++ src/dependenciesViewer/dependenciesDocument.ts
@@ -18,53 +18,102 @@ export class DependenciesDocument {
 	}
 
     get value() {
-		return ` <head>
+		return `<html> <head>
         <title>VivaGraphs test page</title>
         <script src="https://anvaka.github.io/VivaGraphJS/dist/vivagraph.js"></script>
         <script type='text/javascript'>
                   function onLoad() {
-               var graph = Viva.Graph.graph();
+             var graph = Viva.Graph.graph();
 
-// Construct the graph
-graph.addNode('anvaka', {url : 'https://secure.gravatar.com/avatar/91bad8ceeec43ae303790f8fe238164b'});
-graph.addNode('manunt', {url : 'https://secure.gravatar.com/avatar/c81bfc2cf23958504617dd4fada3afa8'});
-graph.addNode('thlorenz', {url : 'https://secure.gravatar.com/avatar/1c9054d6242bffd5fd25ec652a2b79cc'});
-graph.addNode('bling', {url : 'https://secure.gravatar.com/avatar/24a5b6e62e9a486743a71e0a0a4f71af'});
-graph.addNode('diyan', {url : 'https://secure.gravatar.com/avatar/01bce7702975191fdc402565bd1045a8?'});
-graph.addNode('pocheptsov', {url : 'https://secure.gravatar.com/avatar/13da974fc9716b42f5d62e3c8056c718'});
-graph.addNode('dimapasko', {url : 'https://secure.gravatar.com/avatar/8e587a4232502a9f1ca14e2810e3c3dd'});
+var graphics = Viva.Graph.View.svgGraphics(),
+nodeSize = 24;
 
-graph.addLink('anvaka', 'manunt');
-graph.addLink('anvaka', 'thlorenz');
-graph.addLink('anvaka', 'bling');
-graph.addLink('anvaka', 'diyan');
-graph.addLink('anvaka', 'pocheptsov');
-graph.addLink('anvaka', 'dimapasko');
-
-// Set custom nodes appearance
-var graphics = Viva.Graph.View.svgGraphics();
-graphics.node(function(node) {
-       // The function is called every time renderer needs a ui to display node
-       return Viva.Graph.svg('image')
-             .attr('width', 24)
-             .attr('height', 24)
-             .link(node.data.url); // node.data holds custom object passed to graph.addNode();
-    })
-    .placeNode(function(nodeUI, pos){
-        // Shift image to let links go to the center:
-        nodeUI.attr('x', pos.x - 12).attr('y', pos.y - 12);
-    });
-
-var renderer = Viva.Graph.View.renderer(graph,
-    {
-        graphics : graphics
-    });
+var renderer = Viva.Graph.View.renderer(graph, {
+    graphics : graphics
+});
 renderer.run();
+
+graphics.node(function(node) {
+    return Viva.Graph.svg('image')
+        .attr('width', nodeSize)
+        .attr('height', nodeSize)
+        .link('https://secure.gravatar.com/avatar/' + node.data);
+    }).placeNode(function(nodeUI, pos) {
+        nodeUI.attr('x', pos.x - nodeSize / 2).attr('y', pos.y - nodeSize / 2);
+});
+
+
+var createMarker = function(id) {
+        return Viva.Graph.svg('marker')
+                .attr('id', id)
+                .attr('viewBox', "0 0 10 10")
+                .attr('refX', "10")
+                .attr('refY', "5")
+                .attr('markerUnits', "strokeWidth")
+                .attr('markerWidth', "10")
+                .attr('markerHeight', "5")
+                .attr('orient', "auto");
+},
+
+marker = createMarker('Triangle');
+marker.append('path').attr('d', 'M 0 0 L 10 5 L 0 10 z');
+
+var defs = graphics.getSvgRoot().append('defs');
+defs.append(marker);
+
+var geom = Viva.Graph.geom();
+
+graphics.link(function(link){
+    var label = Viva.Graph.svg('text').attr('id','label_'+link.data.id).text(link.data.id);
+            	        graphics.getSvgRoot().childNodes[0].append(label);
+    
+    return Viva.Graph.svg('path')
+        .attr('stroke', 'gray')
+        .attr('marker-end', 'url(#Triangle)')
+        .attr('id', link.data.id);
+    }).placeLink(function(linkUI, fromPos, toPos) {
+        var toNodeSize = nodeSize,
+        fromNodeSize = nodeSize;
+
+        var from = geom.intersectRect(
+            fromPos.x - fromNodeSize / 2, // left
+            fromPos.y - fromNodeSize / 2, // top
+            fromPos.x + fromNodeSize / 2, // right
+            fromPos.y + fromNodeSize / 2, // bottom
+            fromPos.x, fromPos.y, toPos.x, toPos.y)
+        || fromPos;
+
+        var to = geom.intersectRect(
+            toPos.x - toNodeSize / 2, // left
+            toPos.y - toNodeSize / 2, // top
+            toPos.x + toNodeSize / 2, // right
+            toPos.y + toNodeSize / 2, // bottom
+            // segment:
+            toPos.x, toPos.y, fromPos.x, fromPos.y)
+            || toPos;
+
+        var data = 'M' + from.x + ',' + from.y +
+            'L' + to.x + ',' + to.y;
+
+        linkUI.attr("d", data);
+    
+        document.getElementById('label_'+linkUI.attr('id'))
+                	.attr("x", (from.x + to.x) / 2)
+                	.attr("y", (from.y + to.y) / 2);
+    });
+
+// Finally we add something to the graph:
+graph.addNode('anvaka', '91bad8ceeec43ae303790f8fe238164b');
+graph.addNode('indexzero', 'd43e8ea63b61e7669ded5b9d3c2e980f');
+graph.addNode('test', 'd43e8ea63b61e7669ded5b9d3c2e980f');
+graph.addLink('anvaka', 'indexzero', {id : 1});
+graph.addLink('anvaka', 'test', {id : 2});
             }            
         </script>
+         <style type='text/css'>html, body, svg { width: 100%; height: 100%;} </style>
     </head>
     <body onload="onLoad()">
-        
-    </body>`;
+        <div id="graph-container" width='1000px' height='1000px' ></div>
+    </body></html>`;
 	}
 }
